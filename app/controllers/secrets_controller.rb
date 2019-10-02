a173d1c6b66021eabaac23311adfa924392668cc
aparkening <aaron.parkening@gmail.com>
2019-10-02 17:27:29 -0400

    Done. Add login requirements, routes, secrets.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,14 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+  # skip_before_action :require_login, only: [:index]
+
+  def show
+    
+  end
+
+  def secret
+      
+  end
+
+
+end
