1a900ad3b823284a3f77caa7e33d02c56617566e
earthctzn <earthctzn1@gmail.com>
2019-10-06 02:48:16 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def index
+    @name = session[:name]
+  end
+
+  def create
+    
+    if !params[:name] || params[:name].empty?
+      redirect_to login_path, notice: "Please enter your name."
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end 
+
+end
